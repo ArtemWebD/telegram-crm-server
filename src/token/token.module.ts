37ca73c992b180964ee1627cb3687f328000471f
artemWebD <artemtoptun@gmail.com>
2022-07-26 19:46:59 +0700

    initilized token entity in module

--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TokenEntity } from './entities/token.entity';
 import { TokenService } from './token.service';
 
 @Module({
-  providers: [TokenService]
+  imports: [TypeOrmModule.forFeature([TokenEntity])],
+  providers: [TokenService],
 })
 export class TokenModule {}
