f46e799876288e2af9b9aba0e7ad3b56f9ee8350
artemWebD <artemtoptun@gmail.com>
2022-07-26 19:52:13 +0700

    created auth endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(authService: AuthService) {}
+
+  @Post('register')
+  async register() {}
+
+  @Post('login')
+  async login() {}
+
+  @Get('logout')
+  logout() {}
+
+  @Post('refresh')
+  refresh() {}
+}
