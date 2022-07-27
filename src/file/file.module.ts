399cb053b3031631159355da4c266dc7be4f6c06
artemWebD <artemtoptun@gmail.com>
2022-07-27 23:10:23 +0700

    update imports

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FileEntity } from './entities/file.entity';
 import { FileController } from './file.controller';
+import { FileRepository } from './file.repository';
 import { FileService } from './file.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([FileEntity])],
   controllers: [FileController],
-  providers: [FileService]
+  providers: [FileService, FileRepository],
 })
 export class FileModule {}
