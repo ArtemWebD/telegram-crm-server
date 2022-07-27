b1df8b30c37d664c5ab17afe04d13d3bf169bd58
artemWebD <artemtoptun@gmail.com>
2022-07-27 19:52:20 +0700

    implement register method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,28 @@
-import { Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request, Response } from 'express';
 import { AuthService } from './auth.service';
+import { IUser } from './register.strategy';
+
+export interface IAccessToken {
+  accessToken: string;
+}
 
 @Controller('auth')
 export class AuthController {
-  constructor(authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @UseGuards(AuthGuard('register'))
   @Post('register')
-  async register() {}
+  async register(@Req() req: Request, @Res() res: Response): Promise<void> {
+    const user = req.user as IUser;
+    const tokens = await this.authService.register(user);
+    res.cookie('refreshToken', tokens.refreshToken, {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    });
+    res.json({ accessToken: tokens.accessToken });
+  }
 
   @UseGuards(AuthGuard('login'))
   @Post('login')
