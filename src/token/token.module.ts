ba9a9ae2a409429b07ce32d6cc89ab7bf7499325
artemWebD <artemtoptun@gmail.com>
2022-07-27 17:19:23 +0700

    update import and export

--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TokenEntity } from './entities/token.entity';
+import { TokenRepository } from './token.repository';
 import { TokenService } from './token.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TokenEntity])],
-  providers: [TokenService],
+  imports: [TypeOrmModule.forFeature([TokenEntity]), JwtModule, ConfigModule],
+  providers: [TokenService, TokenRepository],
+  exports: [TokenService, TokenRepository],
 })
 export class TokenModule {}
