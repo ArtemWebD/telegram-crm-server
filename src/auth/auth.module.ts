e5753e400547c7b9070e379bb8dfbb0ab9c373db
artemWebD <artemtoptun@gmail.com>
2022-07-27 17:29:08 +0700

    added TokenModule import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { RegisterStrategy } from './register.strategy';
 import { LoginStrategy } from './login.strategy';
+import { TokenModule } from 'src/token/token.module';
 
 @Module({
-  imports: [UserModule, PassportModule],
+  imports: [UserModule, PassportModule, TokenModule],
   providers: [AuthService, RegisterStrategy, LoginStrategy],
   controllers: [AuthController],
 })
