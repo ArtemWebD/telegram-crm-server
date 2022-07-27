f3dc0aa9913b84434d9d7f9f9574ccf695afaa31
artemWebD <artemtoptun@gmail.com>
2022-07-27 20:58:49 +0700

    change access token lifetime

--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -29,7 +29,7 @@ export class TokenService {
     };
   }
 
-  validateAccessToken(token): any | null {
+  validateAccessToken(token: string): any | null {
     try {
       return this.jwtService.verify(token, {
         secret: this.configService.get<string>('JWT_ACCESS_SECRET'),
@@ -39,7 +39,7 @@ export class TokenService {
     }
   }
 
-  validateRefreshToken(token) {
+  validateRefreshToken(token: string): any | null {
     try {
       return this.jwtService.verify(token, {
         secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
