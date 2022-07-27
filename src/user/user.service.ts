6270fd6677d5f65adb0dcf110c8a1aa5e203fa4b
artemWebD <artemtoptun@gmail.com>
2022-07-27 17:00:31 +0700

    added create-method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { IUser } from 'src/auth/register.strategy';
+import { UserEntity } from './entities/user.entity';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  create(user: IUser): Promise<UserEntity> {
+    const { login, password } = user;
+    return this.userRepository.save(login, password);
+  }
+}
