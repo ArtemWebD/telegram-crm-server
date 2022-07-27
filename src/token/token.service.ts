def908dccabc72a01f5a959374b037c9a4121ec5
artemWebD <artemtoptun@gmail.com>
2022-07-27 17:20:39 +0700

    created generate method

--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+
+export interface ITokens {
+  accessToken: string;
+  refreshToken: string;
+}
 
 @Injectable()
-export class TokenService {}
+export class TokenService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  generateTokens(payload: any): ITokens {
+    const accessToken = this.jwtService.sign(payload, {
+      secret: this.configService.get<string>('JWT_ACCESS_SECRET'),
+      expiresIn: '30m',
+    });
+    const refreshToken = this.jwtService.sign(payload, {
+      secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
+      expiresIn: '30d',
+    });
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+}
