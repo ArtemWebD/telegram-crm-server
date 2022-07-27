82dc424a4953814e39195c0c63e0aae0477dd8b2
artemWebD <artemtoptun@gmail.com>
2022-07-27 17:21:32 +0700

    clear imports

--- src/token/token.repository.ts
+++ src/token/token.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, UpdateResult } from 'typeorm';
+import { Repository } from 'typeorm';
 import { TokenEntity } from './entities/token.entity';
 
 @Injectable()
