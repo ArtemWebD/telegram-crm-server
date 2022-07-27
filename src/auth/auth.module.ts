245f1da4df9bb56fb8449669bfc61c9e166c1c0e
artemWebD <artemtoptun@gmail.com>
2022-07-27 16:32:57 +0700

    added strategies in module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { RegisterStrategy } from './register.strategy';
+import { LoginStrategy } from './login.strategy';
 
 @Module({
-  imports: [UserModule],
-  providers: [AuthService],
+  imports: [UserModule, PassportModule],
+  providers: [AuthService, RegisterStrategy, LoginStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
