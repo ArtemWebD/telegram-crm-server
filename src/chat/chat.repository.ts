2ec6cb4a066b231655a4416f8b464f09c64a03ce
artemWebD <artemtoptun@gmail.com>
2022-07-28 11:58:21 +0700

    updated save method

---
+++ src/chat/chat.repository.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FileDto } from 'src/file/dto/file.dto';
+import { FileRepository } from 'src/file/file.repository';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { Repository } from 'typeorm';
+import { ChatEntity } from './entities/chat.entity';
+
+@Injectable()
+export class ChatRepository {
+  constructor(
+    @InjectRepository(ChatEntity)
+    private readonly repository: Repository<ChatEntity>,
+    private readonly fileRepository: FileRepository,
+  ) {}
+
+  async isExist(chatId: number): Promise<boolean> {
+    return !!(await this.repository.findOneBy({ chatId }));
+  }
+
+  async save(
+    user: UserEntity,
+    chatId: number,
+    image?: FileDto,
+  ): Promise<ChatEntity> {
+    const photo = image ? await this.fileRepository.save(image) : null;
+    return this.repository.save({
+      chatId,
+      user,
+      ...(photo && { photo }),
+    });
+  }
+
+  getByChatId(chatId: number): Promise<ChatEntity> {
+    return this.repository.findOneBy({ chatId });
+  }
+}
