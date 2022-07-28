25aed35d2412507a694d0e3cbf230c92e0559dd3
artemWebD <artemtoptun@gmail.com>
2022-07-28 11:45:46 +0700

    added relation argument in save method

--- src/file/file.repository.ts
+++ src/file/file.repository.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { ChatEntity } from 'src/chat/entities/chat.entity';
 import { Repository } from 'typeorm';
 import { FileEntity } from './entities/file.entity';
+
+interface IRelations {
+  chat?: ChatEntity;
+}
 
 @Injectable()
 export class FileRepository {
@@ -10,11 +15,17 @@ export class FileRepository {
     private readonly repository: Repository<FileEntity>,
   ) {}
 
-  save(name: string, mimetype: string, data: Buffer): Promise<FileEntity> {
+  save(
+    name: string,
+    mimetype: string,
+    data: Buffer,
+    relations: IRelations,
+  ): Promise<FileEntity> {
     return this.repository.save({
       name,
       mimetype,
       data: [...data],
+      ...(relations.chat && { chat: relations.chat }),
     });
   }
 
