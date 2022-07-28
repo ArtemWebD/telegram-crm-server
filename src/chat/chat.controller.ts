282e073ceab6be203c61d09b4fcb5616649a5dee
artemWebD <artemtoptun@gmail.com>
2022-07-28 12:19:34 +0700

    created routes

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { AuthorizationGuard, IUserData } from 'src/auth/auth.guard';
+import { DeleteDto } from 'src/common/delete.dto';
+import { ChatRepository } from './chat.repository';
+import { ChatEntity } from './entities/chat.entity';
 
+@UseGuards(AuthorizationGuard)
 @Controller('chat')
-export class ChatController {}
+export class ChatController {
+  constructor(private readonly chatRepository: ChatRepository) {}
+
+  @Get()
+  getChats(
+    @Req() req: Request,
+    @Query('take') take: string,
+    @Query('page') page: string,
+  ): Promise<ChatEntity[]> {
+    const user = req.user as IUserData;
+    return this.chatRepository.getByUser(user.id, +take, +page);
+  }
+
+  @Delete()
+  async remove(@Body() deleteDto: DeleteDto): Promise<void> {
+    await this.chatRepository.remove(deleteDto.id);
+  }
+}
