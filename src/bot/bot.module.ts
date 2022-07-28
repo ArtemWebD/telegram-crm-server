971a9b8238b1e169e701331392034155f0b3b5dc
artemWebD <artemtoptun@gmail.com>
2022-07-28 14:29:44 +0700

    added repository

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BotController } from './bot.controller';
 import { BotService } from './bot.service';
+import { BotEntity } from './entities/bot.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BotEntity])],
   controllers: [BotController],
-  providers: [BotService]
+  providers: [BotService],
 })
 export class BotModule {}
