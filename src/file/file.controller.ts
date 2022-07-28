72d31aeba5945d99bfe5520357975cfa84d1f1fa
artemWebD <artemtoptun@gmail.com>
2022-07-28 11:38:59 +0700

    created get file method

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Response,
+  StreamableFile,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthorizationGuard } from 'src/auth/auth.guard';
+import { FileRepository } from './file.repository';
 
 @Controller('file')
-export class FileController {}
+export class FileController {
+  constructor(private readonly fileRepository: FileRepository) {}
+
+  @UseGuards(AuthorizationGuard)
+  @Get('/:id')
+  async getFile(
+    @Response({ passthrough: true }) res,
+    @Query('id') id: string,
+  ): Promise<StreamableFile> {
+    const fileData = await this.fileRepository.getById(+id);
+    res.set({
+      'Content-Type': fileData.mimetype,
+      'Content-Disposition': `attachment; filename="${fileData.name}"`,
+    });
+    return new StreamableFile(Buffer.from(fileData.data));
+  }
+}
