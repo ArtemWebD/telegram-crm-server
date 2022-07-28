47dcabff7b4cb4151bc361d1313b37464f32d781
artemWebD <artemtoptun@gmail.com>
2022-07-28 12:50:06 +0700

    added repository in exports

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FileModule } from 'src/file/file.module';
 import { ChatController } from './chat.controller';
+import { ChatRepository } from './chat.repository';
 import { ChatService } from './chat.service';
 import { ChatEntity } from './entities/chat.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ChatEntity]), FileModule],
   controllers: [ChatController],
-  providers: [ChatService],
+  providers: [ChatService, ChatRepository],
+  exports: [ChatRepository],
 })
 export class ChatModule {}
