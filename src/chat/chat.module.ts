f89980fffd54a2784800aee929e674ef8bb1b038
artemWebD <artemtoptun@gmail.com>
2022-07-28 11:57:56 +0700

    updated imports

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FileModule } from 'src/file/file.module';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 import { ChatEntity } from './entities/chat.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ChatEntity])],
+  imports: [TypeOrmModule.forFeature([ChatEntity]), FileModule],
   controllers: [ChatController],
   providers: [ChatService],
 })
