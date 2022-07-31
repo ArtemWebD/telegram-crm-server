4e95747baf45e27532d926fbb6589105394b8a28
artemWebD <artemtoptun@gmail.com>
2022-07-31 13:52:29 +0700

    clear imports

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -1,8 +1,10 @@
 import {
   Body,
   Controller,
+  Get,
   Param,
   Post,
+  Query,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
@@ -10,16 +12,21 @@ import { AuthorizationGuard } from 'src/auth/auth.guard';
 import { ChatInterceptor } from 'src/chat/chat.interceptor';
 import { SendMessageDto } from './dto/send-message.dto';
 import { MessageEntity } from './entities/message.entity';
+import { MessageRepository } from './message.repository';
 import { MessageService } from './message.service';
 import { IUpdate } from './update.type';
 
 @Controller('message')
 export class MessageController {
-  constructor(private readonly messageService: MessageService) {}
+  constructor(
+    private readonly messageService: MessageService,
+    private readonly messageRepository: MessageRepository,
+  ) {}
 
   @UseInterceptors(ChatInterceptor)
   @Post('/:token')
   botHandler(@Body() update: IUpdate, @Param('token') token: string) {
+    console.log(update);
     return this.messageService.create(update, token);
   }
 
@@ -27,5 +34,15 @@ export class MessageController {
   @Post()
   send(@Body() sendMessageDto: SendMessageDto) {
     return this.messageService.send(sendMessageDto);
+  }
+
+  @UseGuards(AuthorizationGuard)
+  @Get()
+  getMessages(
+    @Query('chatId') chatId: string,
+    @Query('take') take: string,
+    @Query('page') page: string,
+  ): Promise<MessageEntity[]> {
+    return this.messageRepository.getByChatId(+chatId, +take, +page);
   }
 }
