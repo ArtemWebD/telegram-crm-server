faa499656668ddedb97cfb675e9bce35c26e9909
artemWebD <artemtoptun@gmail.com>
2022-07-31 15:01:34 +0700

    updated imports

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BotModule } from 'src/bot/bot.module';
 import { ChatModule } from 'src/chat/chat.module';
 import { FileModule } from 'src/file/file.module';
+import { SocketModule } from 'src/socket/socket.module';
 import { TokenModule } from 'src/token/token.module';
 import { MessageEntity } from './entities/message.entity';
 import { MessageController } from './message.controller';
@@ -16,6 +17,7 @@ import { MessageService } from './message.service';
     TokenModule,
     BotModule,
     ChatModule,
+    SocketModule,
   ],
   controllers: [MessageController],
   providers: [MessageService, MessageRepository],
