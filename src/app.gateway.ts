88e63e57e64b32215deaaff4c483979cd9ca3ccc
artemWebD <artemtoptun@gmail.com>
2022-07-31 14:18:46 +0700

    created connection and disconnection methods

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,9 +1,33 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import {
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { SocketService } from './socket/socket.service';
 
 @WebSocketGateway()
-export class AppGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+export class AppGateway implements OnGatewayInit {
+  @WebSocketServer()
+  private readonly server: Server;
+  private readonly logger = new Logger('AppGateway');
+
+  constructor(private readonly socketService: SocketService) {}
+
+  afterInit(server: Server) {
+    this.socketService.socket = server;
+    this.logger.log('Socket was inited');
+  }
+
+  @SubscribeMessage('connect')
+  connectionHandler(client: Socket, token: string) {
+    client.join(token);
+  }
+
+  @SubscribeMessage('disconnect')
+  disconnectionHandler(client: Socket, token: string) {
+    client.leave(token);
   }
 }
