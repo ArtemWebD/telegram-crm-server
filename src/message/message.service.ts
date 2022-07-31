8c1c57b5364c5d7734147e7a3dc5ef54d74e4dce
artemWebD <artemtoptun@gmail.com>
2022-07-31 13:53:42 +0700

    fixed bug in getting file

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { ChatRepository } from 'src/chat/chat.repository';
+import { FileDto } from 'src/file/dto/file.dto';
+import { FileEntity } from 'src/file/entities/file.entity';
+import { FileRepository } from 'src/file/file.repository';
 import { FileService } from 'src/file/file.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { SendMessageDto } from './dto/send-message.dto';
@@ -11,20 +15,27 @@ export class MessageService {
   constructor(
     private readonly messageRepository: MessageRepository,
     private readonly fileService: FileService,
+    private readonly fileRepository: FileRepository,
+    private readonly chatRepository: ChatRepository,
   ) {}
 
   async create(update: IUpdate, token: string): Promise<MessageEntity> {
-    const image = update.message.photo
-      ? [...(await this.getPhotos(update.message.photo, token))]
-      : undefined;
-    const text = update.message.text || update.message.caption;
-    const createMessageDto = new CreateMessageDto(
-      text,
-      MessageFrom.user,
-      update.message.from.id,
-      image,
-    );
-    return this.messageRepository.save(createMessageDto);
+    try {
+      const file = await this.getFile(update.message.photo, token);
+      const text = update.message.text || update.message.caption;
+      const { id } = await this.chatRepository.getByChatId(
+        update.message.from.id,
+      );
+      const createMessageDto = new CreateMessageDto(
+        text,
+        MessageFrom.user,
+        id,
+        file ? file.id : undefined,
+      );
+      return this.messageRepository.save(createMessageDto);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   send(sendMessageDto: SendMessageDto): Promise<MessageEntity> {
@@ -34,6 +45,19 @@ export class MessageService {
       sendMessageDto.chatId,
     );
     return this.messageRepository.save(createMessageDto);
+  }
+
+  private async getFile(
+    photo: IPhoto[] | undefined,
+    token: string,
+  ): Promise<FileEntity | undefined> {
+    if (!photo) {
+      return undefined;
+    }
+    const image = photo ? await this.getPhotos(photo, token) : undefined;
+    return this.fileRepository.save(
+      new FileDto('photo.jpg', 'image/jpg', [...image]),
+    );
   }
 
   private getPhotos(photos: IPhoto[], token: string): Promise<Buffer> {
