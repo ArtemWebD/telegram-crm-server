63615dda98952ade8e48f38c1889faa8ef79ae3f
artemWebD <artemtoptun@gmail.com>
2022-07-31 15:01:26 +0700

    added socket emit

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/common';
 import { AuthorizationGuard } from 'src/auth/auth.guard';
 import { ChatInterceptor } from 'src/chat/chat.interceptor';
+import { SocketService } from 'src/socket/socket.service';
 import { SendMessageDto } from './dto/send-message.dto';
 import { MessageEntity } from './entities/message.entity';
 import { MessageRepository } from './message.repository';
@@ -21,13 +22,17 @@ export class MessageController {
   constructor(
     private readonly messageService: MessageService,
     private readonly messageRepository: MessageRepository,
+    private readonly socketService: SocketService,
   ) {}
 
   @UseInterceptors(ChatInterceptor)
   @Post('/:token')
-  botHandler(@Body() update: IUpdate, @Param('token') token: string) {
-    console.log(update);
-    return this.messageService.create(update, token);
+  async botHandler(
+    @Body() update: IUpdate,
+    @Param('token') token: string,
+  ): Promise<boolean> {
+    const message = await this.messageService.create(update, token);
+    return this.socketService.socket.to(token).emit('telegramMessage', message);
   }
 
   @UseGuards(AuthorizationGuard)
