0b6c260ad6eec96e17928f86bddfbfefdcd265df
artemWebD <artemtoptun@gmail.com>
2022-08-11 18:03:51 +0700

    added file module in imports

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FileModule } from 'src/file/file.module';
 import { TokenModule } from 'src/token/token.module';
 import { BotController } from './bot.controller';
 import { BotRepository } from './bot.repository';
@@ -8,7 +9,12 @@ import { BotService } from './bot.service';
 import { BotEntity } from './entities/bot.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BotEntity]), ConfigModule, TokenModule],
+  imports: [
+    TypeOrmModule.forFeature([BotEntity]),
+    ConfigModule,
+    TokenModule,
+    FileModule,
+  ],
   controllers: [BotController],
   providers: [BotService, BotRepository],
   exports: [BotRepository],
