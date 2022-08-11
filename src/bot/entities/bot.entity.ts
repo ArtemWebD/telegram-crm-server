5aa5ee0daf60ec6296b0297ac8180401c9e01f19
artemWebD <artemtoptun@gmail.com>
2022-08-11 14:53:21 +0700

    added new columns

--- src/bot/entities/bot.entity.ts
+++ src/bot/entities/bot.entity.ts
@@ -1,10 +1,12 @@
 import { ChatEntity } from 'src/chat/entities/chat.entity';
+import { FileEntity } from 'src/file/entities/file.entity';
 import { UserEntity } from 'src/user/entities/user.entity';
 import {
   Column,
   Entity,
   ManyToOne,
   OneToMany,
+  OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
@@ -21,6 +23,18 @@ export class BotEntity {
 
   @Column()
   username: string;
+
+  @Column({ nullable: true })
+  chatJoinRequestText?: string;
+
+  @Column({ default: false })
+  approveRequests: boolean;
+
+  @OneToOne(() => FileEntity, (file) => file.bot, {
+    cascade: true,
+    nullable: true,
+  })
+  chatJoinRequestImage?: FileEntity;
 
   @ManyToOne(() => UserEntity, (user) => user.bots)
   user: UserEntity;
